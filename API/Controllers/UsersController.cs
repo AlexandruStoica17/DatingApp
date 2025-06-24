2feeeabc76d4ef1294feeb0b17588733fd3fedb9
Boop <stoica.gh.daniel@student.utcluj.ro>
2025-06-24 17:12:12 +0300

    Sect 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,32 +1,34 @@
 using System;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]  // /api/users
+// [ApiController]
+// [Route("api/[controller]")]  // /api/users
 
-public class UsersController(DataContext context) : ControllerBase
+public class UsersController(DataContext context) : BaseApiController
 {
 
-
+    [AllowAnonymous]
     [HttpGet]
 
     //sincron vs asincron analogie waiter restaurant
     //sincron = waiter takes order then goes to the chef and waits until done, intre timp rest customers don t get served
     //asincron = wwaiter takes order, goes to chef then takes more orders, when order completed waiter takes the food and delivers
-    public async Task <ActionResult<IEnumerable<AppUser>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
         var users = await context.Users.ToListAsync();
 
         return users; //puteam si Ok(users) pt ca tot ok e
     }
 
+    [Authorize]
     [HttpGet("{id:int}")]  // /api/users/id(id adica 1, 2 etc ..)
-    public async Task< ActionResult<AppUser>> GetUser(int id)
+    public async Task<ActionResult<AppUser>> GetUser(int id)
     {
         var user = await context.Users.FindAsync(id);
         if (User == null) return NotFound();

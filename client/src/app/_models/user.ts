4a8e267733675cdd178854e46723278c8e99ac06
Boop <stoica.gh.daniel@student.utcluj.ro>
2025-07-01 22:34:45 +0300

    End of sect 5

---
+++ client/src/app/_models/user.ts
@@ -0,0 +1,5 @@
+export interface User{ //nu e ca in C# sa punem iUser pt interfata
+    username: string; //daca lasam username? : string username poate fi definit ca string sau undefined
+    // (adica proprietatile sunt optionale)
+    token: string;
+}
---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { User } from '../_models/user';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+//angular service: de retinut: singleton, created when the application starts(ex: user refreshes their browser),
+//  good for storing state and making httprequests
+export class AccountService {
+
+
+  private http = inject(HttpClient);  
+  baseUrl= 'https://localhost:5001/api/';
+  currentUser = signal<User | null>(null) //initial value is set to null
+
+  login(model: any){
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map(user => {
+        if(user){
+          localStorage.setItem('user',JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+              })
+    )
+  }
+  
+  register(model: any){
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if(user){
+          localStorage.setItem('user',JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+        return user;
+      })
+    )
+  }
+
+  logout(){
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
+}
